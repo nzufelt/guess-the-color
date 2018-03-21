7b8dfede6996ce7f78701b8f96ee1b903d3060f4
nzufelt <nicholas.zufelt@gmail.com>
2018-03-21 06:49:20 -0400

    Created a footer, cleaned up code style, updated favicon

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+export default class Footer extends Component
+{
+  render() {
+    return (
+      <footer>
+        <p>
+          A simple app created by <a href="https://twitter.com/nicholaszufelt">Nicholas Zufelt</a>.
+        </p>
+      </footer>
+    );
+  }
+}
